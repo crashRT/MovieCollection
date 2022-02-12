a154184f15f21482c8979bce4ff053b80a136345
crashRT <85057127+crashRT@users.noreply.github.com>
2022-02-13 03:01:21 +0900

    モデル修正
    TextFieldが表示されていないのを修正
    
    TextFieldのあとの () を忘れていた

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import collection
+
+admin.site.register(collection)
--- collection/models.py
+++ collection/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 class collection(models.Model):
     title = models.CharField(max_length=200)
-    link = models.TextField
-    creator = models.TextField
+    link = models.TextField(null=True, blank=True)
+    creator = models.TextField(null=True, blank=True)
+    discription = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
