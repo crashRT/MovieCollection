a18c0f34ad6988dfc887484dd53f4cd5a2ac5bf0
crashRT <85057127+crashRT@users.noreply.github.com>
2022-03-20 22:04:54 +0900

    DBの内容を表示
    DB内の映像の一覧表示、詳細表示をできるようにした

--- collection/models.py
+++ collection/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class collection(models.Model):
     title = models.CharField(max_length=200)
     link = models.TextField(null=True, blank=True)
-    creator = models.TextField(null=True, blank=True)
+    creator = models.CharField(max_length=100, null=True, blank=True)
     discription = models.TextField(null=True, blank=True)
 
     def __str__(self):
--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from gc import collect
+from multiprocessing import context
+from operator import methodcaller
+from tempfile import tempdir
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
-def index(requested):
-    return HttpResponse("Hello, world!")
+from .models import collection
+from .forms import add_movie
+
+def index(requested):  
+    collections = collection.objects.order_by('id')
+    context = {
+        'movie_list':collections
+    }
+    return render(requested, 'collection/index.html', context)
+
+def detail(request, movie_id):
+    movie = get_object_or_404(collection, pk=movie_id)
+    return render(request, 'collection/detail.html', {'movie':movie})
+
+def form(request):
+    form = add_movie()
+    context = {'form':form}
+    render(request, 'collection/form.html', context)
 
