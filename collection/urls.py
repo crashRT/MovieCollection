97d941f9ae574625341d46b2f184252e7b03a6a1
crashRT <crashrt.farbauti@gmail.com>
2023-03-04 16:21:59 +0900

    url調整

--- collection/urls.py
+++ collection/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('add', views.add, name='add'),
     path('edit/<int:movie_id>', views.edit, name='edit'),
     path('delete/<int:movie_id>', views.delete_movie, name='delete'),
-    path('list/tag=<str:tagname>', views.taglist, name='taglist'),
+    path('list/tag/<str:tagname>', views.taglist, name='taglist'),
     path('creators', views.creators, name='creators'),
-    path('list/creator=<str:creator>', views.creator_works, name='works'),
+    path('list/creator/<str:creator>', views.creator_works, name='works'),
     path('delete-conf/<int:movie_id>', views.delete_conf, name='delete_conf'),
     path('login', views.Login, name='Login'),
     path('logout', views.Logout, name='Logout')
