259ca0033ae05067b551374d991efe1bc0bc6803
crashRT <crashrt.farbauti@gmail.com>
2023-03-04 23:15:37 +0900

    format

--- collection/views.py
+++ collection/views.py
@@ -14,7 +14,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-#ログイン
+# ログイン
+
+
 def Login(request):
     # POST
     if request.method == 'POST':
@@ -27,10 +29,10 @@ def Login(request):
 
         # ユーザー認証
         if user:
-            #ユーザーアクティベート判定
+            # ユーザーアクティベート判定
             if user.is_active:
                 # ログイン
-                login(request,user)
+                login(request, user)
                 # ホームページ遷移
                 return HttpResponseRedirect(reverse('index'))
             else:
@@ -44,7 +46,7 @@ def Login(request):
         return render(request, 'collection/login.html')
 
 
-#ログアウト
+# ログアウト
 @login_required
 def Logout(request):
     logout(request)
@@ -52,93 +54,103 @@ def Logout(request):
     return HttpResponseRedirect(reverse('Login'))
 
 
-
-#@login_required
-def index(requested):  
+# @login_required
+def index(requested):
     collections = collection.objects.order_by('id')
     taglist = Tag.objects.all()
     context = {
-        'movie_list':collections,
-        'taglist':taglist,
+        'movie_list': collections,
+        'taglist': taglist,
     }
     return render(requested, 'collection/index.html', context)
 
-#@login_required
+# @login_required
+
+
 def taglist(requested, tagname):
-    collections = collection.objects.filter(tags__name__in=[tagname]).distinct()
+    collections = collection.objects.filter(
+        tags__name__in=[tagname]).distinct()
     taglist = Tag.objects.all()
     context = {
-        'movie_list':collections,
-        'taglist':taglist,
-        'tagname':tagname,
+        'movie_list': collections,
+        'taglist': taglist,
+        'tagname': tagname,
     }
     return render(requested, 'collection/index.html', context)
 
-#@login_required
+# @login_required
+
+
 def creators(request):
-    collections = collection.objects.order_by('creator').values_list('creator',flat=True).distinct()
+    collections = collection.objects.order_by(
+        'creator').values_list('creator', flat=True).distinct()
     taglist = Tag.objects.all()
     context = {
-        'collections':collections,
-        'taglist':taglist,
+        'collections': collections,
+        'taglist': taglist,
 
     }
     return render(request, 'collection/creators.html', context)
 
-#@login_required
+# @login_required
+
+
 def creator_works(request, creator):
     works = collection.objects.filter(creator__icontains=creator).distinct()
     taglist = Tag.objects.all()
     context = {
-        'movie_list':works,
-        'taglist':taglist,
-        'creator':creator,
+        'movie_list': works,
+        'taglist': taglist,
+        'creator': creator,
     }
     return render(request, 'collection/index.html', context)
 
-#@login_required
+# @login_required
+
+
 def detail(request, movie_id):
     movie = get_object_or_404(collection, pk=movie_id)
     taglist = Tag.objects.all()
     tags = movie.tags.all()
     context = {
-        'movie':movie,
-        'taglist':taglist,
-        'tags':tags,
+        'movie': movie,
+        'taglist': taglist,
+        'tags': tags,
     }
     return render(request, 'collection/detail.html', context)
+
 
 @login_required
 def add(request):
     if request.method == 'POST':
         form = add_movie(request.POST)
         newLink = form.data['link']
-        if not collection.objects.filter(link = newLink).exists():
+        if not collection.objects.filter(link=newLink).exists():
             if form.is_valid():
                 form.save()
                 return redirect('index')
         else:
-            default_data = {'add_by':User.objects.get(pk=request.user.pk)}
+            default_data = {'add_by': User.objects.get(pk=request.user.pk)}
             form = add_movie(default_data)
             taglist = Tag.objects.all()
             notice = 'その作品は既に登録されています'
             context = {
-                'notice':notice,
-                'form':form,
-                'taglist':taglist,
+                'notice': notice,
+                'form': form,
+                'taglist': taglist,
             }
             return render(request, 'collection/form.html', context)
 
-           
     else:
-        default_data = {'add_by':User.objects.get(pk=request.user.pk)}
+        default_data = {'add_by': User.objects.get(pk=request.user.pk)}
         form = add_movie(default_data)
         taglist = Tag.objects.all()
         context = {
-            'form':form,
-            'taglist':taglist,
+            'form': form,
+            'taglist': taglist,
         }
     return render(request, 'collection/form.html', context)
+
 
 @login_required
 def edit(request, movie_id):
@@ -152,19 +164,21 @@ def edit(request, movie_id):
         form = add_movie(instance=info)
         taglist = Tag.objects.all()
         context = {
-            'form':form,
-            'taglist':taglist,
+            'form': form,
+            'taglist': taglist,
         }
- 
+
     return render(request, 'collection/edit.html', context)
+
 
 @login_required
 def delete_conf(request, movie_id):
     movie = get_object_or_404(collection, pk=movie_id)
     context = {
-        'movie':movie,
+        'movie': movie,
     }
     return render(request, 'collection/delete.html', context)
+
 
 @require_POST
 def delete_movie(request, movie_id):
