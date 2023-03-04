2bab42d5c54d934bbc307ef5829c2dcfcf495cf6
crashRT <crashrt.farbauti@gmail.com>
2023-03-04 16:22:06 +0900

    static file

--- MovieCollection/urls.py
+++ MovieCollection/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles import views
+from django.urls import re_path
+from django.views.static import serve 
 
 urlpatterns = [
     path('', include('collection.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
